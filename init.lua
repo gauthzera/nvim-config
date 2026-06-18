755569a4ead8656546b0712729669ab953705534
Gabriel Ulhoa <gabrielulhoa@outlook.com>
2026-06-18 12:44:38 -0300

    atualização teclas de snippets

--- init.lua
+++ init.lua
@@ -97,15 +97,28 @@ require("lazy").setup({
     },
     
    {
-    "hrsh7th/nvim-cmp", --mostra autcomplete
+    "hrsh7th/nvim-cmp", --mostra autocomplete
     dependencies = {
         "hrsh7th/cmp-nvim-lsp",
         "L3MON4D3/LuaSnip",
     },
 
-    config = function()
+	config = function()
         local cmp = require("cmp")
         local luasnip = require("luasnip")
+
+        -- Navegação de snippets
+        vim.keymap.set({ "i", "s" }, "<C-j>", function() --avança campo do snippet
+            if luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            end
+        end, { silent = true })
+
+        vim.keymap.set({ "i", "s" }, "<C-k>", function() --volta campo do snippet
+            if luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            end
+        end, { silent = true })
 
         cmp.setup({
             snippet = {
@@ -113,22 +126,18 @@ require("lazy").setup({
                     luasnip.lsp_expand(args.body)
                 end,
             },
-            --config de snippets
-            mapping = cmp.mapping.preset.insert({ 
-                ["<Tab>"] = cmp.mapping(function(fallback) --vai para o próximo bloco do snippet
-                    if luasnip.expand_or_jumpable() then
-                        luasnip.expand_or_jump()
-                    elseif cmp.visible() then
+
+            mapping = cmp.mapping.preset.insert({ --proxima sugestão
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
                         cmp.select_next_item()
                     else
                         fallback()
                     end
                 end, { "i", "s" }),
 
-                ["<S-Tab>"] = cmp.mapping(function(fallback) --volta para o bloco anterior
-                    if luasnip.jumpable(-1) then
-                        luasnip.jump(-1)
-                    elseif cmp.visible() then
+                ["<S-Tab>"] = cmp.mapping(function(fallback) --volta sugestão
+                    if cmp.visible() then
                         cmp.select_prev_item()
                     else
                         fallback()
@@ -142,9 +151,9 @@ require("lazy").setup({
                 { name = "nvim_lsp" },
             },
         })
-            end,
-        },
-
+        end,
+    },
+    
     {
         "neovim/nvim-lspconfig",
         config = function()
